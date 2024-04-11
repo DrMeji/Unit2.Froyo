1ea050cba28f7474f49ed4903e46cf35218d57ef
DrMeji <thomasmejia0107@gmail.com>
2024-04-11 00:22:34 -0500

    Updated

---
+++ script.js
@@ -0,0 +1,18 @@
+function processOrder() {
+    const userInput = prompt("Enter your froyo flavors (comma-separated):");
+    const flavors = userInput.split(",");
+    const flavorCounts = countFlavors(flavors);
+    displayFlavorCounts(flavorCounts);
+}
+
+function countFlavors(flavors) {
+    const counts = {};
+    flavors.forEach(flavor => {
+        counts[flavor] = (counts[flavor] || 0) + 1;
+    });
+    return counts;
+}
+
+function displayFlavorCounts(flavorCounts) {
+    console.table(flavorCounts);
+}
